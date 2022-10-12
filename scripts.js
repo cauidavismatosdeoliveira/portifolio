8b1a7393b1483dbd1ddec1e44a9d3adedc22be8b
cauidavismatosdeoliveira <98659275+cauidavismatosdeoliveira@users.noreply.github.com>
2022-10-12 13:42:30 -0300

    commit

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function botao1(){
+    document.getElementById("inicio1").style.display="block";
+    document.getElementById("projeto").style.display = "none";
+    document.getElementById("sobre-mim").style.display = "none";
+}
+function botao2(){
+    document.getElementById("inicio1").style.display="none";
+    document.getElementById("projeto").style.display = "none";
+    document.getElementById("sobre-mim").style.display = "block";
+}
+function botao3(){
+    document.getElementById("inicio1").style.display="none";
+    document.getElementById("projeto").style.display = "block";
+    document.getElementById("sobre-mim").style.display = "none";
+}
+document.getElementById("projeto").style.display = "none";
+document.getElementById("sobre-mim").style.display = "none";
