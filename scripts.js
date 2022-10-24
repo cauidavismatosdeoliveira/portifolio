4c4792020207ae8547c6a977a31bac094515183a
cauidavismatosdeoliveira <98659275+cauidavismatosdeoliveira@users.noreply.github.com>
2022-10-24 11:52:25 -0300

    commit

--- scripts.js
+++ scripts.js
@@ -1,15 +1,15 @@
-function botao1(){
-    document.getElementById("inicio1").style.display="block";
+function botao1() {
+    document.getElementById("inicio1").style.display = "block";
     document.getElementById("projeto").style.display = "none";
     document.getElementById("sobre-mim").style.display = "none";
 }
-function botao2(){
-    document.getElementById("inicio1").style.display="none";
+function botao2() {
+    document.getElementById("inicio1").style.display = "none";
     document.getElementById("projeto").style.display = "none";
     document.getElementById("sobre-mim").style.display = "block";
 }
-function botao3(){
-    document.getElementById("inicio1").style.display="none";
+function botao3() {
+    document.getElementById("inicio1").style.display = "none";
     document.getElementById("projeto").style.display = "block";
     document.getElementById("sobre-mim").style.display = "none";
 }
