68db21d0bfb0a95b9172602ea5afba2db00d1ff7
cauidavismatosdeoliveira <98659275+cauidavismatosdeoliveira@users.noreply.github.com>
2022-11-13 22:22:35 -0300

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,26 @@ function botao1() {
     document.getElementById("inicio1").style.display = "block";
     document.getElementById("projeto").style.display = "none";
     document.getElementById("sobre-mim").style.display = "none";
+    document.getElementById("botao1").style.backgroundColor = "red";
+    document.getElementById("botao2").style.backgroundColor = "blue";
+    document.getElementById("botao3").style.backgroundColor = "blue";
 }
 function botao2() {
     document.getElementById("inicio1").style.display = "none";
     document.getElementById("projeto").style.display = "none";
     document.getElementById("sobre-mim").style.display = "block";
+    document.getElementById("botao1").style.backgroundColor = "blue";
+    document.getElementById("botao2").style.backgroundColor = "red";
+    document.getElementById("botao3").style.backgroundColor = "blue";
 }
 function botao3() {
     document.getElementById("inicio1").style.display = "none";
     document.getElementById("projeto").style.display = "block";
     document.getElementById("sobre-mim").style.display = "none";
+    document.getElementById("botao1").style.backgroundColor = "blue";
+    document.getElementById("botao2").style.backgroundColor = "blue";
+    document.getElementById("botao3").style.backgroundColor = "red";
 }
-
+document.getElementById("botao1").style.backgroundColor = "red";
 document.getElementById("projeto").style.display = "none";
 document.getElementById("sobre-mim").style.display = "none";
